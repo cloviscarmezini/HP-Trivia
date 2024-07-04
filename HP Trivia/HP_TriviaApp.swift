806ce3a5265b037c54aa55d9daac5f786487f316
cloviscarmezini <cloviscarmezini14@gmail.com>
2024-07-03 21:28:28 -0300

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Clovis Carmezini on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
